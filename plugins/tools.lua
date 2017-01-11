66f6d4b37d116652d572b0b1bd04de99d7248012
mosavy <www.mortezamosavy94@gmail.com>
2017-01-11 04:40:29 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -23,6 +23,17 @@ end
 if matches[1] == 'invite' and is_sudo(msg) then
 tg.addChatMember(msg.chat_id_, string.sub(input, 9), 20)
 end
+if matches[1] == 'setrules' and is_owner(msg) or is_momod(msg) then
+text = matches[2]
+pmm == text 
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>Rules supergroup </b>[<code> '..msg.sender_user_id_..'</code> ]<b> saved</b>' , 1, 'html')
+elseif matches[1] == 'rules' and is_owner(msg) or is_momod(msg) then
+if pmm == nil then
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>rules not exist</b> , 1, 'html')
+else
+tg.sendMessage(msg.chat_id_, 0, 1,'<b>Rules supergroup </b>[ <code>'..msg.chat_id_..'</code> ]: \n'..pmm, 1, 'html')
+end
+end	
 end	
 	
 return {
@@ -30,6 +41,8 @@ return {
 		"^[/#!](git pull)$",
 		"^[/#!](echo) (.*)$",
 		"^[/#!](invite)$",
+		"^[/#!](rules)$",
+		"^[/#!](setrules) (.*)$",	
 		"^[/#!](del)$",
 		"^[/#!](ids)$",
   },
