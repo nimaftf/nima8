06d37ea4cebe6e776bbca939458158d725035f9a
mosavy <www.mortezamosavy94@gmail.com>
2017-01-11 16:04:17 +0330

    Update link.lua

--- plugins/link.lua
+++ plugins/link.lua
@@ -9,7 +9,7 @@ end
 	
 if matches[1] == 'link' and is_owner(msg) or is_momod(msg) then
 link1 = redis:get('link'..msg.chat_id_)
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link :</b>\n'..link1 , 1, 'html')
+--tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link :</b>\n'..link1 , 1, 'html')
 end
 end
 end
