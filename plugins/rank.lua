810d3703c3c74920d129d705346c9666bc2d4845
mosavy <www.mortezamosavy94@gmail.com>
2017-01-11 17:49:35 +0330

    Update rank.lua

--- plugins/rank.lua
+++ plugins/rank.lua
@@ -194,16 +194,12 @@ if matches[1] == 'demote' and is_owner(msg) then
 	end
 
 end
-	if matches[1] == 'rules' and is_owner(msg) or is_momod(msg) and addgroup then
-rules1 = redis:get('rules'..msg.chat_id_)
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules :</b>\n'..rules1 , 1, 'html')
-end
+	
 end
 return {
   patterns = {
     "^[#!/](setowner)$",
  "^[#!/](owner)$",
-		"^[#!/](rules)$",
  "^[#!/](modlist)$",
 	"^[#!/](setowner) (.+)$",
     "^[#!/](promote)$",
