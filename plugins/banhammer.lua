f15a3a55a7b7f489949208df0d3c5d50262ba82f
mosavy <www.mortezamosavy94@gmail.com>
2017-01-12 03:09:34 +0330

    Update banhammer.lua

--- plugins/banhammer.lua
+++ plugins/banhammer.lua
@@ -2,25 +2,25 @@ local function run(msg ,matches)
 	if matches[1] == 'kick' and is_owner(msg) or is_momod(msg) then
 	if msg.reply_to_message_id_~=0 then
 		tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
-		tg.changeChatMemberStatus(chat_id_, msg.sender_user_id_, 'Kicked')
+		tg.changeChatMemberStatus(msg.chat_id_, msg.sender_user_id_, 'Kicked')
 	elseif msg.text:match("@(.*)") then
 	     tg.sendMessage(msg.chat_id, 0, 0,  'user [ @'..msg.text:match("@(.*)")..' ] kicked', 0)
-		tg.changeChatMemberStatus(chat_id, msg.text:match("@(.*)"), 'Kicked')	
+		tg.changeChatMemberStatus(msg.chat_id_, msg.text:match("@(.*)"), 'Kicked')	
 	elseif tonumber(matches[2]) then
          tg.sendMessage(msg.chat_id, 0, 0,  '[ '..matches[2]..' ] kicked', 0)
-		tg.changeChatMemberStatus(chat_id, data.id_, 'Kicked')	
+		tg.changeChatMemberStatus(msg.chat_id_, data.id_, 'Kicked')	
 	end
 	end
 		if matches[1] == 'ban' and is_owner(msg) or is_momod(msg) then
 	if msg.reply_to_message_id_~=0 then
 		tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
-		tg.changeChatMemberStatus(chat_id_, msg.sender_user_id_, 'Banned')
+		tg.changeChatMemberStatus(msg.chat_id_, msg.sender_user_id_, 'Banned')
 	elseif msg.text:match("@(.*)") then
 	     tg.sendMessage(msg.chat_id, 0, 0,  'user [ @'..msg.text:match("@(.*)")..' ] Banned', 0)
-		tg.changeChatMemberStatus(chat_id, msg.text:match("@(.*)"), 'Banned')	
+		tg.changeChatMemberStatus(msg.chat_id_, msg.text:match("@(.*)"), 'Banned')	
 	elseif tonumber(matches[2]) then
          tg.sendMessage(msg.chat_id, 0, 0,  '[ '..matches[2]..' ] Banned', 0)
-		tg.changeChatMemberStatus(chat_id, data.id_, 'Banned')	
+		tg.changeChatMemberStatus(msg.chat_id_, data.id_, 'Banned')	
 	end
 	end
 end
