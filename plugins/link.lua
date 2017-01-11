53537416c48be8de75595c9c78e41829a6f9c8a8
mosavy <www.mortezamosavy94@gmail.com>
2017-01-11 16:33:37 +0330

    Update link.lua

--- plugins/link.lua
+++ plugins/link.lua
@@ -8,7 +8,7 @@ tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
 end]]		
 if matches[1] == 'link' and is_owner(msg) or is_momod(msg) then
 link1 = redis:get('link'..msg.chat_id_)
---tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link :</b>\n'..link1 , 1, 'html')
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link :</b>\n'..link1 , 1, 'html')
 end
 end
 end
