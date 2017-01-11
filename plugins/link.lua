15bf098b10d11e31a9d34b71b622c872ef4d7ae5
mosavy <www.mortezamosavy94@gmail.com>
2017-01-11 17:25:57 +0330

    Update link.lua

--- plugins/link.lua
+++ plugins/link.lua
@@ -7,16 +7,13 @@ if matches[1] == 'link' and is_owner(msg) or is_momod(msg) then
 link1 = redis:get('link'..msg.chat_id_)
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link :</b>\n'..link1 , 1, 'html')
 end
-if matches[1] == 'rules' and is_owner(msg) or is_momod(msg) then
-rules1 = redis:get('rules'..msg.chat_id_)
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules :</b>\n'..rules1 , 1, 'html')
-end		
+
 end
 end
 return {
   patterns = {
-		"^[/#!](rules)$",
-		"^[/#!](link)$",
+		​		
+"^[/#!](link)$",
  },
   run = run
 }
