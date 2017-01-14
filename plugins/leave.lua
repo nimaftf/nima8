7396c89158fcdfee209e5756fbba972873a8a783
mosavy <www.mortezamosavy94@gmail.com>
2017-01-14 15:12:15 +0330

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -1,9 +1,9 @@
 local function run(msg, matches)
 local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
-
+bot_id = 288058778
 	if matches[1] == 'leave' and is_sudo(msg) and addgroup or not addgroup then
-	     chat_leave(msg.chat_id_, 288058778)
+	     chat_leave(msg.chat_id_, bot_id)
     end
 end	
 return {
