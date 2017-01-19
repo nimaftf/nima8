f914e2503ef66151b2ad429cc9d2c6fb7bf3e202
mosavy <www.mortezamosavy94@gmail.com>
2017-01-19 23:46:43 +0330

    Update modlist.lua

--- plugins/modlist.lua
+++ plugins/modlist.lua
@@ -14,8 +14,10 @@ local function run(msg, matches)
 local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id_)]
 if addgroup then
+if matches[1] == 'modlist' then               
 return modlist(msg)
 end
+end               
 end
 return {
  patterns = {
