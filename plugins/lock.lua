d884795d1bab6a9c88435042fa981a76e0ecad7c
mosavy <www.mortezamosavy94@gmail.com>
2017-01-20 17:16:00 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -660,6 +660,7 @@ tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
   end
 end
 --if data[tostring(target)] then  
+local group = load_data('bot/group.json')
 if group[tostring(target)]['settings']['num_msg_max'] then
 NUM_MSG_MAX = tonumber(group[tostring(target)]['settings']['num_msg_max'])
  print('custom'..NUM_MSG_MAX) 
