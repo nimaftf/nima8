6f6a684ec3f3e539b48c37674f4b000a420eebb9
mosavy <www.mortezamosavy94@gmail.com>
2017-01-20 21:53:55 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -26,8 +26,8 @@ pm = pm..'\n21- <code>Lock Voice</code> : '..(group[tostring(target)]['settings'
 pm = pm..'\n22- <code>Lock Document</code> : '..(group[tostring(target)]['settings']['mute_document'] or 'no')..''
 pm = pm..'\n23- <code>Lock Audio</code> : '..(group[tostring(target)]['settings']['mute_audio'] or 'no')..'\n-------------------------------------------'
 pm = pm..'\n24- <code>Mute All</code> : '..(group[tostring(target)]['settings']['mute_all'] or 'no')..''
-pm = pm..'\n25- <code>Flood sensitivity</code> : '..NUM_MSG_MAX..''
---pm = pm..'\n25- <code>Flood sensitivity</code> : '..(tonumber(group[tostring(target)]['settings']['num_msg_max']))''
+--pm = pm..'\n25- <code>Flood sensitivity</code> : '..NUM_MSG_MAX..''
+pm = pm..'\n25- <code>Flood sensitivity</code> : '..(tonumber(group[tostring(target)]['settings']['num_msg_max']))''
 	
 pm = pm..'\n-------------------------------------------\n\n<b>Channel: </b>@LeaderCh'
 tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
@@ -701,8 +701,8 @@ pm = pm..'\n17- <code>Lock Voice</code> : '..group[tostring(target)]['settings']
 pm = pm..'\n18- <code>Lock Document</code> : '..group[tostring(target)]['settings']['mute_document']..''
 pm = pm..'\n19- <code>Lock Audio</code> : '..group[tostring(target)]['settings']['mute_audio']..'\n-------------------------------------------'
 pm = pm..'\n20- <code>Mute All</code> : '..group[tostring(target)]['settings']['mute_all']..''
-pm = pm..'\n25- <code>Flood sensitivity</code> : '..NUM_MSG_MAX..''
---pm = pm..'\n25- <code>Flood sensitivity</code> : '..(tonumber(group[tostring(target)]['settings']['num_msg_max']))''
+--pm = pm..'\n25- <code>Flood sensitivity</code> : '..NUM_MSG_MAX..''
+pm = pm..'\n25- <code>Flood sensitivity</code> : '..(tonumber(group[tostring(target)]['settings']['num_msg_max']))''
 	
 pm = pm..'\n-------------------------------------------\n<b>Channel: </b>@LeaderCh'
 tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
