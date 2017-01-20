122674e30a81c296f0c81b210afe05d7e177c8d5
mosavy <www.mortezamosavy94@gmail.com>
2017-01-20 21:44:13 +0330

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -2,8 +2,8 @@ local function run(msg, matches)
 local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
 	if matches[1] == 'leave' and is_sudo(msg) and addgroup or not addgroup then
-	      tg.changeChatMemberStatus(chat, robot, 'Left')
-		  --tg.changeChatMemberStatus(msg.chat_id_, robot, 'Left') 
+	      --tg.changeChatMemberStatus(chat, robot, 'Left')
+		  tg.changeChatMemberStatus(msg.chat_id_, robot, 'Left') 
     end
 end	
 return {
