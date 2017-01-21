9c830d24d1903756987f2333b16b9072c005e3f0
mosavy <www.mortezamosavy94@gmail.com>
2017-01-21 23:02:38 +0330

    Update setlink.lua

--- plugins/setlink.lua
+++ plugins/setlink.lua
@@ -6,9 +6,9 @@ redis:set('link'..msg.chat_id_,nil)
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Link Deleted</b>' , 1, 'html')
 end      
 if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) and addgroup then
-local is_link = msg.text:match("^([https?://w]*.?telegram.me/joinchat/%S+)$") or msg.text.match("^([https?://w]*.?t.me/joinchat/%S+)$")   
+--local is_link = msg.text:match("^([https?://w]*.?telegram.me/joinchat/%S+)$") or msg.text.match("^([https?://w]*.?t.me/joinchat/%S+)$")   
 --matches[2] = is_link
-redis:set('link'..msg.chat_id_,'https://telegram.me/joinchat/'..matches[2] or 'https://t.me/joinchat/'..matches[2][2])
+redis:set('link'..msg.chat_id_,'https://telegram.me/joinchat/'..matches[2] or 'https://t.me/joinchat/'..matches[2])
 if matches[2] then
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
 --else                        
