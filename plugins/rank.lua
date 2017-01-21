cc8bc78faa3e894789fbe071efaa11259dc24603
mosavy <www.mortezamosavy94@gmail.com>
2017-01-21 03:55:30 +0330

    Update rank.lua

--- plugins/rank.lua
+++ plugins/rank.lua
@@ -1,8 +1,8 @@
 local function modlist(msg)
      local i = 1
 if next(group[tostring(msg.chat_id_)]['moderators']) == nil then 
-pmmm = '<b>No moderator in this group</b>'
-tg.sendMessage(msg.chat_id_, 0, 1, pmmm , 1, 'html')
+pm = '<b>No moderator in this group</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm , 1, 'html')
 end
   local message = '<b>List of moderators :</b> \n'
   for k,v in pairs(group[msg.chat_id_]['moderators']) do
