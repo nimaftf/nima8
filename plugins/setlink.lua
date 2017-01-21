9947344a0190e539ffd67ccb06b104a1690e9198
mosavy <www.mortezamosavy94@gmail.com>
2017-01-21 17:33:59 +0330

    Update setlink.lua

--- plugins/setlink.lua
+++ plugins/setlink.lua
@@ -6,16 +6,16 @@ redis:set('link'..msg.chat_id_,nil)
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Link Deleted</b>' , 1, 'html')
 end      
 if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) and addgroup then
-if msg.content_.text_ then
 local is_link = msg.text:match("[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/") or msg.text.match("[Hh][Tt][Tt][Pp][Ss]://[Tt].[Mm][Ee]/")   
 --matches[2] = is_link
 redis:set('link'..msg.chat_id_,matches[2])
 if matches[2] == is_link then
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
-end                        
+else                        
 if matches[2] == not is_link then
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Not Link</b>', 1, 'html')
 end	
+end      
 end    
 end
 return {
