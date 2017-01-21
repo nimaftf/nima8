9552bf8675dc6bfc48cfa2d14a82ea09c593f2f6
mosavy <www.mortezamosavy94@gmail.com>
2017-01-22 00:23:18 +0330

    Update setlink.lua

--- plugins/setlink.lua
+++ plugins/setlink.lua
@@ -8,11 +8,12 @@ end
 if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) and addgroup then
 --local is_link = msg.text:match("^([https?://w]*.?telegram.me/joinchat/%S+)$") or msg.text.match("^([https?://w]*.?t.me/joinchat/%S+)$")   
 --matches[2] = is_link
-redis:set('link'..msg.chat_id_,'https://telegram.me/joinchat/'..matches[2] or 'https://t.me/joinchat/'..matches[2])
+redis:set('link'..msg.chat_id_,'https://'..matches[2])
 --if matches[2] then
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
-else                        
---if matches[2] == not is_link then
+end    
+local is_links = 'https://'  
+if matches[2] == not is_links then
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Not Link</b>', 1, 'html')
 --end	
 --end      
@@ -21,8 +22,8 @@ end
 return {
   patterns = {
 "^[/#!](setlink) (.*)$",
-"^[#!/](setlink) https://telegram.me/joinchat/(.*)$",
-"^[#!/](setlink) https://t.me/joinchat/(.*)$",    
+--"^[#!/](setlink) https://telegram.me/joinchat/(.*)$",
+--"^[#!/](setlink) https://t.me/joinchat/(.*)$",    
 "^[/#!](clean) (.*)$",            
 --"^([https?://w]*.?t.me/joinchat/%S+)$",
 --"^([https?://w]*.?telegram.me/joinchat/%S+)$",
