59f5f4baee19a8da26bde9f6dd0a0a95369db458
mosavy <www.mortezamosavy94@gmail.com>
2017-01-22 03:09:25 +0330

    Update link.lua

--- plugins/link.lua
+++ plugins/link.lua
@@ -4,7 +4,7 @@ local addgroup = group[tostring(msg.chat_id)]
 if addgroup then
 		
 if matches[1] == 'link' and is_owner(msg) or is_momod(msg) then
-link1 = redis:get('link'..msg.chat_id_)
+local link1 = redis:get('link'..msg.chat_id_)
 if not link1 then 
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>please First set a link for group with using ~></b> /setlink link \n<b>Instead <code>\'link\'</code>, Put your link group</b>' , 1, 'html')
 else
