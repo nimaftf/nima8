52f0c6bef302503786b97f296db7859b82ec85c2
mosavy <www.mortezamosavy94@gmail.com>
2017-01-22 00:07:47 +0330

    Update setrules.lua

--- plugins/setrules.lua
+++ plugins/setrules.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
 local group = load_data('bot/group.json')	
 local addgroup = group[tostring(msg.chat_id)]
-if matches[1] == 'setrules' and is_owner(msg) or is_momod(msg) and groupa then
+if matches[1] == 'setrules' and is_owner(msg) or is_momod(msg) and addgroup then
 redis:set('rules'..msg.chat_id_,matches[2])
 tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules Saved</b>', 1, 'html')
 end			
