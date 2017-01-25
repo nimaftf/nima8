848ac22ea584beaf4dd3d568516415b2254a3397
mosavy <www.mortezamosavy94@gmail.com>
2017-01-26 01:01:50 +0330

    Update rmsg1.lua

--- plugins/rmsg1.lua
+++ plugins/rmsg1.lua
@@ -11,7 +11,7 @@ local addgroup = group[tostring(msg.chat_id)]
        --if is_sudo(msg) or is_momod(msg) or is_owner(msg) and addgroup then
           if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
              pm = '<b>More than</b> <code>1</code> <b>and less than</b> <code>40</code>'
-             tg.sendMessage(msg.chat_id_, data.msg.id_, 1, pm, 1, 'html')
+             tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
              else
           tdcli_function ({
     ID = "GetChatHistory",
@@ -20,9 +20,11 @@ local addgroup = group[tostring(msg.chat_id)]
     offset_ = 0,
     limit_ = tonumber(matches[2])
   }, rmmsg, nil)
-             pm ='<code>'..matches[2]..'</code> <b>Last Msgs Has Been Removed</b>'
-             tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
+            --[[ pm ='<code>'..matches[2]..'</code> <b>Last Msgs Has Been Removed</b>'
+             tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')]]
          end
+        pm ='<code>'..matches[2]..'</code> <b>Last Msgs Has Been Removed</b>'
+             tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
      --end
 --end
 end
