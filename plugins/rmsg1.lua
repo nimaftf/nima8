8df7226b3a0f2c0a3414bc817669c1fe2764b76a
mosavy <www.mortezamosavy94@gmail.com>
2017-01-27 19:03:21 +0330

    Update rmsg1.lua

--- plugins/rmsg1.lua
+++ plugins/rmsg1.lua
@@ -9,20 +9,21 @@ local addgroup = group[tostring(msg.chat_id)]
     if matches[1] == 'rmsg' then
     --if msg.chat_id_:match("^-40") then
        if is_sudo(msg) or is_owner(msg) and addgroup then
-          if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
+         --[[ if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
              pm = '<b>More than</b> <code>1</code> <b>and less than</b> <code>40</code>'
              tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
-             else
+             else]]
           tdcli_function ({
     ID = "GetChatHistory",
     chat_id_ = msg.chat_id_,
     from_message_id_ = 0,
     offset_ = 0,
-    limit_ = tonumber(matches[2])
+    limit_ = 100 --tonumber(matches[2])
   }, rmmsg, nil)
-             pm ='<code>'..matches[2]..'</code> <b>Last Msgs Has Been Removed</b>'
+             --pm ='<code>'..matches[2]..'</code> <b>Last Msgs Has Been Removed</b>'
+             pm ='<b> Message Has Been Removed</b>'   
              tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
-         end
+         --end
      end
 --end
 end
@@ -31,6 +32,7 @@ end
 return {
     patterns = {
         '^[!#/]([Rr]msg) (%d+)$'
+        '^[!#/]([Rr]msg)$'
     },
     run = run
 }
