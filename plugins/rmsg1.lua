df267a55676e53ad100c50fe360eb1b7c3e66d2b
mosavy <www.mortezamosavy94@gmail.com>
2017-01-27 22:05:00 +0330

    Update rmsg1.lua

--- plugins/rmsg1.lua
+++ plugins/rmsg1.lua
@@ -19,12 +19,12 @@ local addgroup = group[tostring(msg.chat_id)]
     chat_id_ = msg.chat_id_,
     from_message_id_ = 0,
     offset_ = 0,
-    limit_ = tonumber(matches[2])--  ÷ 2
+    limit_ = tonumber(matches[2])  / 2
   }, rmmsg, nil)
-		local matches[2] ÷ 2 = w		
+		local matches[2] / 2 = w		
               local id1 = msg.id_ - 1
         for i= id1 - w , id1 do
-		--local w = i ÷ 2			
+		--local w = i / 2			
 		tg.deleteMessages(msg.chat_id_, {[0] = i })  
 	end				
              --pm ='<code>'..matches[2]..'</code> <b>Last Msgs Has Been Removed</b>'
