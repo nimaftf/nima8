d70da329d376e37c8f4a2f79341248e3e9f8bc57
mosavy <www.mortezamosavy94@gmail.com>
2017-01-29 03:15:26 +0330

    Update filter.lua

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -44,7 +44,23 @@ if matches[1] == 'filter' and is_momod(msg) or is_owner(msg) then
  return filter_word(msg, matches[2]) 
 end 
 if matches[1] == 'unfilter' and is_momod(msg) or is_owner(msg) then 
-return unfilter_word(msg, matches[2]) 
+--return unfilter_word(msg, matches[2]) 
+     local group = load_data('bot/group.json')
+  if not group[tostring(msg.chat_id_)]['filterlist'] then
+    group[tostring(msg.chat_id_)]['filterlist'] = {}
+    save_data(_config.group.data, group)
+    end
+      if group[tostring(msg.chat_id_)]['filterlist'][word] then
+      group[tostring(msg.chat_id_)]['filterlist'][(word)] = nil
+       save_data(_config.group.data, group)
+
+text2 = '*Word [*'..word..'*] removed from filtered words list*'
+tg.sendMessage(msg.chat_id_, 0, 1, text2 , 1, 'md')
+      else
+ 
+text3 = '*Word *'..word..'* is not filtered*'
+tg.sendMessage(msg.chat_id_, 0, 1, text3 , 1, 'md')
+   end 
 end 
 if matches[1] == 'filterlist' and is_momod(msg) or is_owner(msg) then 
 return filter_list(msg) 
