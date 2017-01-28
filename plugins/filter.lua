4cbd272cf98795db0bdb18edbea0b458a8d044f9
mosavy <www.mortezamosavy94@gmail.com>
2017-01-29 03:17:06 +0330

    Update filter.lua

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -18,9 +18,9 @@ tg.sendMessage(msg.chat_id_, 0, 1, text1 , 1, 'md')
 
 end
 
-local function unfilter_word(msg, word)
+--[[local function unfilter_word(msg, word)
 
- --[[local group = load_data('bot/group.json')
+ local group = load_data('bot/group.json')
   if not group[tostring(msg.chat_id_)]['filterlist'] then
     group[tostring(msg.chat_id_)]['filterlist'] = {}
     save_data(_config.group.data, group)
@@ -35,8 +35,8 @@ tg.sendMessage(msg.chat_id_, 0, 1, text2 , 1, 'md')
  
 text3 = '*Word *'..word..'* is not filtered*'
 tg.sendMessage(msg.chat_id_, 0, 1, text3 , 1, 'md')
-   end]]
-end
+   end
+end]]
 local function run(msg, matches)
 local addgroup = group[tostring(msg.chat_id_)]
 if addgroup then
