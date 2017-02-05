5d1ecae4e6569d9aad637e2c6d1304642b68598e
mosavy <www.mortezamosavy94@gmail.com>
2017-02-05 15:36:39 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -14,8 +14,10 @@ end
 
 if matches[1] == 'ids' and addgroup and is_owner(msg) or is_momod(msg) then
 if msg.reply_to_message_id_ ~= 0 then
+local user = sender_user_id_			
 tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
-tg.sendMessage(msg.chat_id_, 0, 1,'<b>ID: </b>[ '..sender_user_id_..' ]', 1, 'html')
+--tg.sendMessage(msg.chat_id_, 0, 1,'<b>ID: </b>[ '..sender_user_id_..' ]', 1, 'html')
+return '<b>ID: </b>[ '..user..' ]'			
 end
 end
 end
