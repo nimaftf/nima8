d8395b6fae806ae693b3a1a3bd5d956e69d85d93
mosavy <www.mortezamosavy94@gmail.com>
2017-02-10 00:41:02 +0330

    Update lock_bots.lua

--- plugins/lock_bots.lua
+++ plugins/lock_bots.lua
@@ -25,7 +25,7 @@ local function pre_process(msg)
 		end
 	end
 	
-	local is_bot_msg = msg.text:match("!!!tdservice:")
+	--[[local is_bot_msg = msg.text:match("!!!tdservice:")
 	if is_bot_msg then
 		local group = load_data('bot/group.json')
 		local lock_bots = group[tostring(msg.chat_id_)]["settings"]["lock_bot"] 
@@ -35,9 +35,9 @@ local function pre_process(msg)
 			user_id_ = is_bot_msg 
 			}, del_bots, {chat_id=msg.chat_id_})
 		end
-	end	
+	end]]
 	
-    --[[if msg.content_.text_ then
+    if msg.content_.text_ then
 		 local group = load_data('bot/group.json')
 		 local lock_bots = group[tostring(msg.chat_id_)]["settings"]["lock_bots"] 
 		 if lock_bots == 'yes' then
@@ -46,7 +46,7 @@ local function pre_process(msg)
 			 user_id_ = msg.sender_user_id_
 			 }, del_bots, {chat_id=msg.chat_id_})
 		 end
-	    end]]	
+	    end	
 end
 
 return { 
