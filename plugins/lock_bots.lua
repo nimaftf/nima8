8346b6c50b858c383f004a72af58f7ae36e14d7a
mosavy <www.mortezamosavy94@gmail.com>
2017-02-10 00:39:01 +0330

    Update lock_bots.lua

--- plugins/lock_bots.lua
+++ plugins/lock_bots.lua
@@ -13,17 +13,19 @@ tg.changeChatMemberStatus(chat_id, user_id, 'Kicked')
 end]]
 
 local function pre_process(msg)
-	if is_momod(msg) or is_owner(msg) then return end
+	if not is_momod(msg) or not is_owner(msg) then 
 	function del_bots(arg, data)
 		if data.username_ then
 			if string.sub(data.username_:lower(), -3) == 'bot' then
-				kick_user(data.id_, arg.chat_id)
+				--kick_user(data.id_, arg.chat_id)
+				tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })	
 				--chat_del_user(msg.chat_id_, msg.sender_user_id_) 
 			end
 		end
+		end
 	end
 	
-	--[[local is_bot_msg = msg.text:match("!!!tdservice:")
+	local is_bot_msg = msg.text:match("!!!tdservice:")
 	if is_bot_msg then
 		local group = load_data('bot/group.json')
 		local lock_bots = group[tostring(msg.chat_id_)]["settings"]["lock_bot"] 
@@ -33,9 +35,9 @@ local function pre_process(msg)
 			user_id_ = is_bot_msg 
 			}, del_bots, {chat_id=msg.chat_id_})
 		end
-	end]]	
+	end	
 	
-    if msg.content_.text_ then
+    --[[if msg.content_.text_ then
 		 local group = load_data('bot/group.json')
 		 local lock_bots = group[tostring(msg.chat_id_)]["settings"]["lock_bots"] 
 		 if lock_bots == 'yes' then
@@ -44,8 +46,7 @@ local function pre_process(msg)
 			 user_id_ = msg.sender_user_id_
 			 }, del_bots, {chat_id=msg.chat_id_})
 		 end
-	    end
-	
+	    end]]	
 end
 
 return { 
