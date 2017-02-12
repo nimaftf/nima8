a6c8ccc1de5e3bc3bb2f98345d935f19d4b36de2
mosavy <www.mortezamosavy94@gmail.com>
2017-02-12 12:14:53 +0330

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -8,10 +8,10 @@ local function run(msg, matches)
     local msg_id = msg.id_
     local group = load_data('bot/group.json')
     local addgroup = group[tostring(msg.chat_id)]
-
-    if matches[1] == 'rmsg' and addgroup and is_owner(msg) or is_momod(msg) then
+    if addgroup and is_owner(msg) then
+    if matches[1] == 'rmsg' then
                 if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
-                    pm = '*More than* `1` *and less than* `40`'
+                    pm = '*More than* 1 *and less than* 40'
                    tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md') 
                 else
                     tg.getChatHistory(chat_id, msg_id, 0, tonumber(matches[2]), delmsg, nil)
@@ -19,6 +19,7 @@ local function run(msg, matches)
                     tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md')
                 end
      end
+end
 end
 
 return {
