88ccdacd04118de8f4649b98bfd82a6f98ab442e
mosavy <www.mortezamosavy94@gmail.com>
2017-02-12 22:51:01 +0330

    Update checks.lua

--- plugins/checks.lua
+++ plugins/checks.lua
@@ -7,11 +7,13 @@ else
 local group = load_data('bot/group.json')
 
 local group_lock_bot = group[tostring(msg.chat_id)]['settings']['lock_bot']
-if group_lock_bot == 'yes'  and  data.type_.ID == "UserTypeBot" then
+--if group_lock_bot == 'yes'  and  data.type_.ID == "UserTypeBot" then
+if msg.content_.members_[0].username_ and msg.content_.members_[0].username_:match("[Bb][Oo][Tt]$") then   
+if group_lock_bot == 'yes' then       
 --tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
-kick_user(arg.chat_id,data.id_)
+ kick_user(msg.chat_id_,msg.sender_user_id_)
 end
-
+end
 end
 end
 end
