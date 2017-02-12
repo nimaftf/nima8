089fad256b7c210cfb0ad159d7dabd8244c66840
mosavy <www.mortezamosavy94@gmail.com>
2017-02-12 12:08:08 +0330

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -1,4 +1,4 @@
-local function delmsg (arg,data)
+local group = local function delmsg (arg,data)
     for k,v in pairs(data.messages_) do
         tg.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
     end
@@ -6,20 +6,19 @@ end
 local function run(msg, matches)
     local chat_id = msg.chat_id_
     local msg_id = msg.id_
-    if matches[1] == 'rmsg' then
     local group = load_data('bot/group.json')
     local addgroup = group[tostring(msg.chat_id)]
-        if addgroup and is_owner(msg) and momod(msg) then 
+
+    if matches[1] == 'rmsg' and addgroup and is_owner(msg) or is_momod(msg) then
                 if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
-                    pm = '<b>More than</b> <code>1</code> <b>and less than</b> <code>40</code>'
-                    tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html') 
+                    pm = '*More than* `1` *and less than* `40`'
+                   tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md') 
                 else
                     tg.getChatHistory(chat_id, msg_id, 0, tonumber(matches[2]), delmsg, nil)
-                    pm ='<b>Message Has Been Removed</b>'
-                    tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+                    pm = '*Message Has Been Removed*'
+                    tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md')
                 end
-        end
-    end
+     end
 end
 
 return {
